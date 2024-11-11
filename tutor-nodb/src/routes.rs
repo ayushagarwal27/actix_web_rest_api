eebc9e8043eae79ec759af43a2437cc80c0e1f58
Ayush <ayushagarwal.dev@gmail.com>
2024-11-11 10:05:17 +0530

    add add course by tutor post api endpoint

--- tutor-nodb/src/routes.rs
+++ tutor-nodb/src/routes.rs
@@ -5,3 +5,6 @@ pub fn general_routes(cfg: &mut web::ServiceConfig) {
     cfg.route("/health", web::get().to(health_check_handler));
 }
 
+pub fn course_routes(cfg: &mut web::ServiceConfig) {
+    cfg.service(web::scope("/courses").route("/", web::post().to(new_course)));
+}
